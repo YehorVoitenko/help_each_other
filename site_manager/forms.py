b767937894fc8f1f0855b6fa23d8d4a9858a60d8
YehorVoitenko <egorka.voitenko@gmail.com>
2022-08-17 21:56:30 +0300

    Update site style

--- site_manager/forms.py
+++ site_manager/forms.py
@@ -6,6 +6,15 @@ class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'description']
-        widgets = {'title': TextInput(attrs={'placeholder': "Enter title"}),
-                   'description': Textarea(attrs={'placeholder': "Enter description"})
+        widgets = {'title': TextInput(attrs={
+            'placeholder': "Enter title",
+            'class': "form-control",
+            'id': 'title',
+
+        }),
+                   'description': Textarea(attrs={
+                       'placeholder': "Enter description",
+                       'class': "form-control",
+                       'id': 'description'
+                   })
                    }
