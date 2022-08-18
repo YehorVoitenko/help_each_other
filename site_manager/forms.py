9b9bc1d709b297c9d4c9a58aa13d1a7a9c0b3f84
YehorVoitenko <egorka.voitenko@gmail.com>
2022-08-18 11:22:00 +0300

    Added new forms

--- site_manager/forms.py
+++ site_manager/forms.py
@@ -5,16 +5,50 @@ from django.forms import ModelForm, TextInput, Textarea, ImageField
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'description']
-        widgets = {'title': TextInput(attrs={
-            'placeholder': "Enter title",
-            'class': "form-control",
-            'id': 'title',
+        fields = ['name', 'surname', 'telephone_number',
+                  'title', 'description', 'city', 'key_help_words']
 
-        }),
-                   'description': Textarea(attrs={
-                       'placeholder': "Enter description",
-                       'class': "form-control",
-                       'id': 'description'
-                   })
-                   }
+        widgets = {
+            'name': TextInput(attrs={
+                'placeholder': 'Enter your name',
+                'class': "form-control",
+                'id': 'name'
+            }),
+
+            'surname': TextInput(attrs={
+                'placeholder': 'Enter your surname',
+                'class': "form-control",
+                'id': 'surname'
+            }),
+
+            'telephone_number': TextInput(attrs={
+                'placeholder': 'Enter your telephone number',
+                'class': "form-control",
+                'id': 'telephone_number'
+            }),
+
+            'title': TextInput(attrs={
+                'placeholder': "Enter title",
+                'class': "form-control",
+                'id': 'title',
+
+            }),
+
+            'description': Textarea(attrs={
+               'placeholder': "Enter description (200 symbols)",
+               'class': "form-control",
+               'id': 'description'
+                   }),
+
+            'city': TextInput(attrs={
+               'placeholder': "Enter city, you live",
+               'class': "form-control",
+               'id': 'city'
+                   }),
+
+            'key_help_words': TextInput(attrs={
+               'placeholder': "A few key words, for easier search example: table, chair, toys",
+               'class': "form-control",
+               'id': 'key_help_words'
+                   }),
+        }
--- site_manager/models.py
+++ site_manager/models.py
@@ -4,8 +4,15 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField('Enter title', max_length=50)
-    description = models.TextField('Enter description')
+    name = models.CharField('Enter your name', max_length=50, null=False)
+    surname = models.CharField('Enter your surname', max_length=50, null=False)
+    telephone_number = models.CharField('Enter your telephone number', max_length=50, null=False)
+
+    title = models.CharField('Enter title', max_length=50, null=False)
+    description = models.TextField('Enter description', null=False)
+    city = models.CharField('Enter city, you live', max_length=50, null=False)
+    key_help_words = models.CharField('Enter your telephone number', max_length=50, null=False)
+
     image = models.ImageField()
     date = models.DateTimeField(auto_now_add=True)
 
